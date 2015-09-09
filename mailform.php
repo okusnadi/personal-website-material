f3657c0dd0284338827a1f4ea9c043a54395a795
Titto Thomas <tittoambadan@gmail.com>
2015-09-10 02:32:35 +0530

    Added redirect from php page

--- mailform.php
+++ mailform.php
@@ -14,7 +14,7 @@ else :
 endif;
 
 // This is in the PHP file and sends a Javascript alert to the client
-header('Location: index.html');
 echo "<script type='text/javascript'>alert('$message');</script>";
+header('Location: index.html');
 exit;
 ?>
