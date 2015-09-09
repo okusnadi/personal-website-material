acadf214927d87da1bacc85d4f6c3e160363ed4d
Titto Thomas <tittoambadan@gmail.com>
2015-09-10 02:15:57 +0530

    Added the PHP file to send mail

---
+++ mailform.php
@@ -0,0 +1,13 @@
+$to      = 'tittoambadan@gmail.com';
+$subject = 'Personal page message from ' .  $_POST['name'];
+$message = $_POST['message'];
+$headers = 'From: ' .  $_POST['email'] . "\r\n" .'X-Mailer: PHP/' . phpversion();
+
+$send = mail($to, $subject, $message, $headers);
+
+// this will help you to get the status mail sent or not
+if($send) :
+   echo "Email sent";
+else :
+    echo "Email sending failed";
+endif;
